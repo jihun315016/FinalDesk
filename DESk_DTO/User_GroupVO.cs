beab180dc8abf1508e5ec19ff0d0bafe515d4711
준모 김 <rnsdlsdmltk@gmail.com>
2022-07-20 16:00:14 +0900

    dd

---
+++ DESk_DTO/User_GroupVO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESK_DTO
+{
+    public class User_GroupVO
+    {
+        public int User_Group_No { get; set; }
+        public string User_Group_Type { get; set; }
+        public string Create_Time { get; set; }
+        public int Create_User_No { get; set; }
+        public int Create_User_Name { get; set; }
+        public string Update_Time { get; set; }
+        public int Update_User_No { get; set; }
+        public int Update_User_Name { get; set; }
+
+    }
+}
