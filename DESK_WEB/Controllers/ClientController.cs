d2839a4083d4eb5b08b58d0ad05c748061301240
우성 정 <wsms2004@hanmail.net>
2022-07-20 20:36:05 +0900

    거래처 API 등록,수정,삭제(테스트)

--- DESK_WEB/Controllers/ClientController.cs
+++ DESK_WEB/Controllers/ClientController.cs
@@ -43,5 +43,83 @@ namespace DESK_WEB.Controllers
 
         }
 
+
+        //https://localhost:44393/api/Client/{id}
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult ClientsInfo(int no)
+        {
+            ClientDAC db = new ClientDAC();
+            ClientVO user = db.GetClientInfo(no);
+
+            ResMessage<ClientVO> result = new ResMessage<ClientVO>()
+            {
+                ErrCode = (user == null) ? -9 : 0,
+                ErrMsg = (user == null) ? "해당하는 정보가 없습니다." : "S",
+                Data = user
+            };
+
+            return Ok(result);
+        }
+
+        //POST : https://localhost:44393/api/Client/SaveClient
+        [HttpPost]
+        [Route("SaveClient")]
+        public IHttpActionResult SaveClient(ClientVO user)
+        {
+            try
+            {
+                ClientDAC db = new ClientDAC();
+                bool flag = db.SaveClient(user);
+
+                ResMessage result = new ResMessage()
+                {
+                    ErrCode = (!flag) ? -9 : 0,
+                    ErrMsg = (!flag) ? "저장중 오류발생" : "S"
+                };
+
+                return Ok(result);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(err.Message);
+
+                return Ok(new ResMessage()
+                {
+                    ErrCode = -9,
+                    ErrMsg = "서비스 관리자에게 문의하시기 바랍니다."
+                });
+            }
+        }
+
+        //GET: https://localhost:44393/api/Client/DelClient/{no}
+        [HttpGet]
+        [Route("DelClient/{no}")]
+        public IHttpActionResult DeleteUser(int no)
+        {
+            try
+            {
+                ClientDAC db = new ClientDAC();
+                bool flag = db.DeleteClient(no);
+
+                ResMessage result = new ResMessage()
+                {
+                    ErrCode = (!flag) ? -9 : 0,
+                    ErrMsg = (!flag) ? "삭제중 오류발생" : "S"
+                };
+
+                return Ok(result);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(err.Message);
+
+                return Ok(new ResMessage()
+                {
+                    ErrCode = -9,
+                    ErrMsg = "서비스 관리자에게 문의하시기 바랍니다."
+                });
+            }
+        }
     }
 }
--- DESK_WEB/Models/ClientDAC.cs
+++ DESK_WEB/Models/ClientDAC.cs
@@ -38,5 +38,92 @@ namespace DESK_WEB.Models
                 return list;
             }
         }
+
+        public ClientVO GetClientInfo(int no)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(strConn);
+                cmd.CommandText = @"select Client_Code, 
+                                           Client_Name, 
+                                           Client_Type,                                           
+                                           CONVERT(VARCHAR(20), Create_Time, 20) Create_Time,
+                                           Create_User_No,
+                                           CONVERT(VARCHAR(20), Update_Time, 20) Update_Time,
+                                           Update_User_No 
+                                    from TB_Client
+                                    where Client_Code=@Client_Code";
+                cmd.Parameters.AddWithValue("@Client_Code", no);
+
+                cmd.Connection.Open();
+                List<ClientVO> list = Helper.DataReaderMapToList<ClientVO>(cmd.ExecuteReader());
+                cmd.Connection.Close();
+
+                if (list != null && list.Count > 0)
+                    return list[0];
+                else
+                    return null;
+            }
+        }
+
+        public bool SaveClient(ClientVO client)
+        {
+            using (SqlCommand cmd = new SqlCommand())            
+            {
+                cmd.Connection = new SqlConnection(strConn);
+                cmd.CommandText = @"INSERT INTO TB_Client (Client_Code, Client_Name, Client_Type, Create_Time)
+                                    VALUES (@Client_Code, @Client_Name, @Client_Type, @Create_Time)";
+
+                cmd.Parameters.AddWithValue("@Client_Code", client.Client_Code);
+                cmd.Parameters.AddWithValue("@Client_Name", client.Client_Name);
+                cmd.Parameters.AddWithValue("@Client_Type", client.Client_Type);
+                cmd.Parameters.AddWithValue("@Create_Time", DateTime.Now);
+
+                cmd.Connection.Open();
+                int iRowAffect = cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+
+                return (iRowAffect > 0);
+            }
+        }
+
+        public bool UpdateClient(ClientVO client)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(strConn);
+                cmd.CommandText = @"UPDATE TB_Client SET Client_Name = @Client_Name,
+                                                         Client_Type= @Client_Type
+                                    WHERE Client_Code= @Client_Code";
+
+                cmd.Parameters.AddWithValue("@Client_Code", client.Client_Code);
+                cmd.Parameters.AddWithValue("@Client_Name", client.Client_Name);
+                cmd.Parameters.AddWithValue("@Client_Type", client.Client_Type);
+
+                cmd.Connection.Open();
+                int iRowAffect = cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+
+                return (iRowAffect > 0);
+            }
+        }
+
+        public bool DeleteClient(int clientNO)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(strConn);
+                cmd.CommandText = @"UPDATE TB_Client SET Is_Delete = 'Y'
+                                    WHERE Client_Code= @Client_Code";
+
+                cmd.Parameters.AddWithValue("@Client_Code", clientNO);
+
+                cmd.Connection.Open();
+                int iRowAffect = cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+
+                return (iRowAffect > 0);
+            }
+        }
     }
 }
