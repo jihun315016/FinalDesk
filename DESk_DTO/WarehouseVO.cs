9223281619e17bfb9e85be05c0456df2a5599891
우성 정 <wsms2004@hanmail.net>
2022-07-20 14:36:08 +0900

    창고VO 추가

---
+++ DESk_DTO/WarehouseVO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESK_DTO
+{
+    class WarehouseVO
+    {
+        //Warehouse_Code, Warehouse_Name, Warehouse_Address, Create_Time, Create_User_No
+        //, Update_Time, Update_User_No, Is_Delete
+        public string Warehouse_Code { get; set; }
+        public string Warehouse_Name { get; set; }
+        public string Warehouse_Address { get; set; }
+        public string Create_Time { get; set; }
+        public int Create_User_No { get; set; }
+        public string Update_Time { get; set; }
+        public int Update_User_No { get; set; }
+        public string Is_Delete { get; set; }
+    }
+}
