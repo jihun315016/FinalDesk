9fa9366cd0a3be8f43b66ed5abfa6b6cc93e4e6b
우성 정 <wsms2004@hanmail.net>
2022-07-20 19:01:43 +0900

    메뉴 디자인 수정

--- DESK_MES/Forms/frmMain.Designer.cs
+++ DESK_MES/Forms/frmMain.Designer.cs
@@ -62,7 +62,7 @@ namespace DESK_MES
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(241, 150);
+            this.panel1.Size = new System.Drawing.Size(241, 140);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -71,7 +71,7 @@ namespace DESK_MES
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(241, 150);
+            this.pictureBox1.Size = new System.Drawing.Size(241, 140);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
