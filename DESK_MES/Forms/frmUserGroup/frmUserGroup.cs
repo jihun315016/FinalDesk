4d640f70a6c89dd80c16e0bf153c1c280b580e33
준모 김 <rnsdlsdmltk@gmail.com>
2022-07-22 02:09:19 +0900

    임시저장용

--- DESK_MES/Forms/frmUserGroup/frmUserGroup.cs
+++ DESK_MES/Forms/frmUserGroup/frmUserGroup.cs
@@ -278,6 +278,7 @@ namespace DESK_MES
             }
         }
 
+        //엑셀버튼
         private void button3_Click(object sender, EventArgs e)
         {
             SaveFileDialog dlg = new SaveFileDialog();
