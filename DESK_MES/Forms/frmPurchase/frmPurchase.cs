496294e9968076f8638d9bf5a68229c1aa6026cb
우성 정 <wsms2004@hanmail.net>
2022-07-26 12:47:07 +0900

    발주 등록

--- DESK_MES/Forms/frmPurchase/frmPurchase.cs
+++ DESK_MES/Forms/frmPurchase/frmPurchase.cs
@@ -7,27 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DESK_DTO;
 
 namespace DESK_MES
 {
     public partial class frmPurchase : FormStyle_1
     {
+        PurchaseService srv;
+        int purchaseNo = 0;
+        List<PurchaseVO> purchaseList;
+        List<PurchaseDetailVO> purchaseDetailList;
+
+
         public frmPurchase()
         {
             InitializeComponent();
             label1.Text = "발주 관리";
         }
+        private void frmPurchase_Load(object sender, EventArgs e)
+        {
+            srv = new PurchaseService();
 
+            DataGridUtil.SetInitGridView(dataGridView1);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "발주코드", "Purchase_No", colWidth: 120);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "거래처번호", "Client_Code", colWidth: 230);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "거래처명", "Client_Name", colWidth: 60);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "발주등록일", "Purchase_Date", colWidth: 80);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "발주상태", "Purchase_State", colWidth: 60);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "입고예정일", "IncomingDue_date", colWidth: 120);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "입고상태", "Is_Incoming", colWidth: 60);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "입고일", "Incoming_Date", colWidth: 120);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "등록일자", "Create_Time", colWidth: 60);
+            //DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "등록사용자", "Create_User_No", colWidth: 80);
+            //DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "수정일자", "Update_Time", colWidth: 60);
+            //DataGridUtil.SetDataGridViewColumn_TextBox(dataGridView1, "수정사용자", "Update_User_No", colWidth: 60);
+            
+            
+            LoadData();
+        }
+        private void LoadData()
+        {
+            purchaseList = srv.GetPurchaseList();
+            dataGridView1.DataSource = purchaseList;
+        }
         private void btnAdd_Click(object sender, EventArgs e)
         {
             PopPurchaseRegister pop = new PopPurchaseRegister(); ;
-            pop.ShowDialog();
+            if (pop.ShowDialog() == DialogResult.OK)
+            {
+                LoadData();
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnModify_Click(object sender, EventArgs e)
         {
             PopPurchaseModify pop = new PopPurchaseModify();
-            pop.ShowDialog();
+            if (pop.ShowDialog() == DialogResult.OK)
+            {
+                LoadData();
+            }
         }
+
+        private void btnIncoming_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
---
+++ DESK_MES/Service/PurchaseService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DESK_DTO;
+using System.Data.SqlClient;
+
+namespace DESK_MES
+{
+    public class PurchaseService
+    {
+        public List<PurchaseVO> GetPurchaseList()  // 주문 정보 가져오기
+        {
+            PurchaseDAC dac = new PurchaseDAC();
+            List<PurchaseVO> result = dac.GetPurchaseList();
+            dac.Dispose();
+
+            return result;
+        }
+
+        public List<ProductVO> GetProductListForPurchase()
+        {
+            PurchaseDAC dac = new PurchaseDAC();
+            List<ProductVO> result = dac.GetProductListForPurchase();
+            dac.Dispose();
+
+            return result;
+        }
+
+        public bool RegisterPurchase(PurchaseVO purchase, List<PurchaseDetailVO> purchaseList)
+        {
+            PurchaseDAC dac = new PurchaseDAC();
+            bool result = dac.RegisterPurchase(purchase, purchaseList);
+            dac.Dispose();
+
+            return result;
+        }
+    }
+}
