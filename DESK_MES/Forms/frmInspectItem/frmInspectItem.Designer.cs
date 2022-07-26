5506f4051c342761e2db3be9136d2e5d5bedcc40
jihun <hun90531@google.com>
2022-07-26 21:16:18 +0900

    검사 항목 조회, 검색

--- DESK_MES/Forms/frmInspectItem/frmInspectItem.Designer.cs
+++ DESK_MES/Forms/frmInspectItem/frmInspectItem.Designer.cs
@@ -32,8 +32,7 @@ namespace DESK_MES
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInspectItem));
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.txtInspectNoSearch = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -64,6 +63,7 @@ namespace DESK_MES
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.txtInspectNameSearch = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel7.SuspendLayout();
             this.panel5.SuspendLayout();
@@ -108,6 +108,10 @@ namespace DESK_MES
             // 
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
+            // btnOpenDetail
+            // 
+            this.btnOpenDetail.Click += new System.EventHandler(this.btnOpenDetail_Click);
+            // 
             // comboBox1
             // 
             this.comboBox1.Size = new System.Drawing.Size(100, 25);
@@ -120,8 +124,8 @@ namespace DESK_MES
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.textBox10);
-            this.groupBox2.Controls.Add(this.comboBox4);
+            this.groupBox2.Controls.Add(this.txtInspectNameSearch);
+            this.groupBox2.Controls.Add(this.txtInspectNoSearch);
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.label7);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -131,38 +135,30 @@ namespace DESK_MES
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
-            // textBox10
+            // txtInspectNoSearch
             // 
-            this.textBox10.Location = new System.Drawing.Point(83, 32);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(108, 25);
-            this.textBox10.TabIndex = 34;
-            // 
-            // comboBox4
-            // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(271, 32);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(108, 25);
-            this.comboBox4.TabIndex = 33;
+            this.txtInspectNoSearch.Location = new System.Drawing.Point(114, 32);
+            this.txtInspectNoSearch.Name = "txtInspectNoSearch";
+            this.txtInspectNoSearch.Size = new System.Drawing.Size(108, 25);
+            this.txtInspectNoSearch.TabIndex = 34;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(197, 36);
+            this.label8.Location = new System.Drawing.Point(228, 36);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(68, 17);
+            this.label8.Size = new System.Drawing.Size(94, 17);
             this.label8.TabIndex = 32;
-            this.label8.Text = "||  값 유형";
+            this.label8.Text = "||  검사 항목명";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(12, 36);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(65, 17);
+            this.label7.Size = new System.Drawing.Size(96, 17);
             this.label7.TabIndex = 30;
-            this.label7.Text = "검사 항목";
+            this.label7.Text = "검사 항목 번호";
             // 
             // tableLayoutPanel1
             // 
@@ -524,6 +520,13 @@ namespace DESK_MES
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // txtInspectNameSearch
+            // 
+            this.txtInspectNameSearch.Location = new System.Drawing.Point(328, 32);
+            this.txtInspectNameSearch.Name = "txtInspectNameSearch";
+            this.txtInspectNameSearch.Size = new System.Drawing.Size(108, 25);
+            this.txtInspectNameSearch.TabIndex = 35;
+            // 
             // frmInspectItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
@@ -555,10 +558,9 @@ namespace DESK_MES
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox txtInspectNoSearch;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.TextBox textBox9;
@@ -587,5 +589,6 @@ namespace DESK_MES
         private System.Windows.Forms.ImageList imageList2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtInspectNameSearch;
     }
 }
--- DESK_MES/Forms/frmInspectItem/frmInspectItem.cs
+++ DESK_MES/Forms/frmInspectItem/frmInspectItem.cs
@@ -35,14 +35,22 @@ namespace DESK_MES
         {
             InspectList = InspectSrv.GetInspectItemList();
 
+            //dtpCreateTime.Format = DateTimePickerFormat.Custom;
+            //dtpCreateTime.CustomFormat = " ";
+            //dtpUpdateTime.Format = DateTimePickerFormat.Custom;
+            //dtpUpdateTime.CustomFormat = " ";
+
+            comboBox1.Items.AddRange(new string[] { "검색 조건", "검사 항목 번호", "검사 항목명" });
+            comboBox1.SelectedIndex = 0;
+
             DataGridUtil.SetInitGridView(dgvList);
-            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "검사 항목 번호", "Inspect_No");
-            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "검사 항목명", "Inspect_Name");
-            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "타겟값", "Target");
-            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "상한값", "USL");
-            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "하한값", "LSL");
-            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "등록 시간", "Create_Time");
-            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "등록 사용자", "Create_User_Name");
+            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "검사 항목 번호", "Inspect_No", colWidth: 160);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "검사 항목명", "Inspect_Name", colWidth: 170);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "타겟값", "Target", colWidth: 130);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "상한값", "USL", colWidth: 130);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "하한값", "LSL", colWidth: 130);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "등록 시간", "Create_Time", colWidth: 200);
+            DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "등록 사용자", "Create_User_Name", colWidth: 180);
             DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "등록 사용자 번호", "Create_User_No", isVisible: false);
             DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "수정 사용자 번호", "Update_User_No", isVisible: false);
             DataGridUtil.SetDataGridViewColumn_TextBox(dgvList, "수정 사용자", "Update_Time", isVisible: false);
@@ -53,7 +61,36 @@ namespace DESK_MES
         {
             List<InspectItemVO> list = InspectList.Where(p => 1 == 1).ToList();
 
+            // 상세 검색
+            if (panel5.Visible)
+            {
+                if (!string.IsNullOrWhiteSpace(txtInspectNoSearch.Text.Trim()))
+                    list = list.Where(i => i.Inspect_No.ToString().ToLower().Contains(txtInspectNoSearch.Text.ToLower())).ToList();
+
+                if (!string.IsNullOrWhiteSpace(txtInspectNameSearch.Text.Trim()))
+                    list = list.Where(i => i.Inspect_Name.ToLower().Contains(txtInspectNameSearch.Text.ToLower())).ToList();
+            }
+            // 일반 검색
+            else
+            {
+                // 품번 검색
+                if (comboBox1.SelectedIndex == 1)
+                    list = list.Where(i => i.Inspect_No.ToString().Contains(textBox1.Text.ToLower())).ToList();
+
+                // 품명 검색
+                else if (comboBox1.SelectedIndex == 2)
+                    list = list.Where(i => i.Inspect_Name.ToLower().Contains(textBox1.Text.ToLower())).ToList();
+            }
+
             dgvList.DataSource = list;
+        }
+
+        private void btnOpenDetail_Click(object sender, EventArgs e)
+        {
+            if (panel5.Visible)
+                comboBox1.Enabled = textBox1.Enabled = false;
+            else
+                comboBox1.Enabled = textBox1.Enabled = true;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
