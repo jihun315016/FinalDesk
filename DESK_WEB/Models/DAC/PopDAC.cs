e222f5f2c93abcdbdc651223c866d2e5abb57bb8
준모 김 <rnsdlsdmltk@gmail.com>
2022-07-29 17:11:49 +0900

    POP 디자인 기반1

--- DESK_WEB/Models/DAC/PopDAC.cs
+++ DESK_WEB/Models/DAC/PopDAC.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace DESK_WEB.Models
 {
     public class PopDAC
     {
+        string strConn;
+
+        public PopDAC()
+        {
+            strConn = WebConfigurationManager.ConnectionStrings["prjDB"].ConnectionString;
+        }
+        public PopVO GetUserLogin(int id)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = new SqlConnection(strConn);
+                    cmd.CommandText = @"USP_PopLogin";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue(@"@_UsID", id);
+
+                    cmd.Connection.Open();
+                    List<PopVO> list = Helper.DataReaderMapToList<PopVO>(cmd.ExecuteReader());
+                    cmd.Connection.Close();
+
+                    return list[0];
+                }
+            }
+            catch (Exception err)
+            {
+                return null;
+            }
+        }
     }
 }
