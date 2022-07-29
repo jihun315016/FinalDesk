2e7e065e61f8fbc63c40debb119414e1cad982f7
우성 정 <wsms2004@hanmail.net>
2022-07-29 09:47:58 +0900

    거래처 수정 중 (DAC, Service MES에 새로 생성)

--- DESK_MES/Forms/frmClient/frmClient.Designer.cs
+++ DESK_MES/Forms/frmClient/frmClient.Designer.cs
@@ -457,7 +457,7 @@ namespace DESK_MES
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(809, 389);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // btnExcel
             // 
