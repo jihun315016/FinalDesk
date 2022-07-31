b8dc35a6b128c911e30c5c07a0a08d6d60a38306
jihun <hun90531@google.com>
2022-07-31 16:07:01 +0900

    중간 백업

--- DESK_WEB/Controllers/MainController.cs
+++ DESK_WEB/Controllers/MainController.cs
@@ -13,5 +13,10 @@ namespace DESK_WEB.Controllers
         {
             return View();
         }
+
+        public ActionResult GetMenuList()
+        {
+            return View();
+        }
     }
 }
