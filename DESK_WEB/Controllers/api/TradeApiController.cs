fe60cea21dba4d001a6b5b8f84e0208663bc1e44
jihun <hun90531@google.com>
2022-08-04 23:07:54 +0900

    웹 중간백업

--- DESK_WEB/Controllers/api/TradeApiController.cs
+++ DESK_WEB/Controllers/api/TradeApiController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DESK_WEB.Models;
+using DESK_WEB.Models.DAC;
+using DESK_WEB.Models.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,32 @@ namespace DESK_WEB.Controllers.api
 {
     public class TradeApiController : ApiController
     {
+        public IHttpActionResult GetPurchaseList()
+        {
+            string startDate = "2022-07-01";
+            string endDate = "2022-08-04";
+            try
+            {
+                TradeDAC dac = new TradeDAC();
+                List<WebPurchaseVO> list = dac.GetPurchaseList(startDate, endDate);
+
+                ResMessage<List<WebPurchaseVO>> result = new ResMessage<List<WebPurchaseVO>>()
+                {
+                    ErrCode = (list == null) ? -9 : 0,
+                    ErrMsg = (list == null) ? "조회중 오류발생" : "S",
+                    Data = list
+                };
+
+                return Ok(result);
+            }
+            catch (Exception err)
+            {
+                return Ok(new ResMessage()
+                {
+                    ErrCode = -9,
+                    ErrMsg = "서비스 관리자에게 문의하시기 바랍니다."
+                });
+            }
+        }
     }
 }
---
+++ DESK_WEB/Utility/LoggingUtil.cs
@@ -0,0 +1,35 @@
+﻿using DESK_WEB.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESK_WEB.Utility
+{
+    public class LoggingUtil
+    {
+        static string baseUrl = ConfigurationManager.AppSettings["apiURL"];
+
+        /// <summary>
+        /// Author : 강지훈
+        /// Web api를 통한 에러 로깅
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static bool LoggingError(LoggingMsgVO msg)
+        {
+            string url = $"{baseUrl}api/log/writeErrLog";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage resMsg = client.PostAsJsonAsync(url, msg).Result;
+
+            if (resMsg.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+    }
+}
