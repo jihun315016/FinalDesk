ce4358ba4f7776a0c0fca20474826fec64cfb2b6
준모 김 <rnsdlsdmltk@gmail.com>
2022-08-07 15:53:53 +0900

    pop 수정1

--- DESK_WEB/Models/DAC/PopDAC.cs
+++ DESK_WEB/Models/DAC/PopDAC.cs
@@ -16,6 +16,11 @@ namespace DESK_WEB.Models
         {
             strConn = WebConfigurationManager.ConnectionStrings["prjDB"].ConnectionString;
         }
+        /// <summary>
+        /// 유저 아이디로 로그인 진행 및 유저 내역 가져오기
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public PopVO GetUserLogin(int id)
         {
             try
@@ -32,6 +37,38 @@ namespace DESK_WEB.Models
                     cmd.Connection.Close();
 
                     return list[0];
+                }
+            }
+            catch (Exception err)
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// 유저 그룹 넘버로 작업지시 가져오기
+        /// </summary>
+        /// <param name="gCode"></param>
+        /// <returns></returns>
+        public List< PopVO> GetWorkList(int gCode)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = new SqlConnection(strConn);
+                    cmd.CommandText = @"select Production_No, Work_Code,Work_Group_Code as User_Group_No,[User_Group_Name], Production_Equipment_Code,Equipment_Name, convert(nvarchar(20), Start_Due_Date,20)as Start_Due_Date, Work_State, Material_Lot_Input_State
+                                        from [dbo].[TB_WORK] w inner join [dbo].[TB_USER_GROUP] ug on w.[Work_Group_Code]=ug.User_Group_No
+                                                       inner join [dbo].[TB_EQUIPMENT] eq on w.Production_Equipment_Code = eq.[Equipment_No]
+                                        where [Work_Group_Code] = @@Work_Group_Code
+                                        order by Work_Code";
+                                            cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Work_Group_Code", gCode);
+
+                    cmd.Connection.Open();
+                    List<PopVO> list = Helper.DataReaderMapToList<PopVO>(cmd.ExecuteReader());
+                    cmd.Connection.Close();
+
+                    return list;
                 }
             }
             catch (Exception err)
--- DESK_WEB/Models/DTO/PopVO.cs
+++ DESK_WEB/Models/DTO/PopVO.cs
@@ -7,11 +7,20 @@ namespace DESK_WEB.Models
 {
     public class PopVO
     {
+        //login
         public int User_No { get; set; }
         public string User_Name { get; set; }
         public int User_Group_No { get; set; }
         public string User_Group_Name { get; set; }
         public int User_Group_Type { get; set; }
         public string Auth_Name { get; set; }
+        //pop uc
+        public int Production_No { get; set; }
+        public int Production_Equipment_Code { get; set; }
+        public string Equipment_Name { get; set; }
+        public string Start_Due_Date { get; set; }
+        public string Work_State { get; set; }
+        public string Material_Lot_Input_State { get; set; }
+
     }
 }
